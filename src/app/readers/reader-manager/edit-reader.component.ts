111ff28aa9fdfd2dde27bd24b198747903f039c4
HubDavidoff <filip.davidoski@gmail.com>
2020-02-27 13:49:35 +0100

    implemented edit-reader component

--- src/app/readers/reader-manager/edit-reader.component.ts
+++ src/app/readers/reader-manager/edit-reader.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IReader } from 'src/app/models/readers';
+import { ReaderService } from 'src/app/services/reader.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-reader',
@@ -7,11 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditReaderComponent implements OnInit {
 
-  
+  reader : IReader;
+  id : string;
+  errorMessage : string;
+  readerName : string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private context : ReaderService, private route : ActivatedRoute, private router : Router) {
+    this.id = this.route.snapshot.paramMap.get("id");  
   }
 
+  set name (value : string){
+    this.readerName = value;
+  }
+
+  ngOnInit() {
+    this.context.getReaderById(this.id)
+    .subscribe(data=> this.reader = data,
+      error=>this.errorMessage = <any>error);
+  }
+
+  submit(){
+    this.reader.name = this.readerName;
+    this.context.editReader(this.reader)
+    .subscribe(data=>console.log(data),
+    error=>this.errorMessage = <any>error);
+    this.router.navigate(['/index']);
+  }
+
+  cancel(){
+    this.router.navigate(['/index']);
+  }
 }
